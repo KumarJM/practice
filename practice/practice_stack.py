9050c4faa7fea96f1cdbe7b57694b02168ddab71
vijay <mvkumar.admin@gmail.com>
2021-07-08 20:40:52 -0500

    Initial Commit

--- practice/practice_stack.py
+++ practice/practice_stack.py
@@ -4,12 +4,23 @@ from aws_cdk import core as cdk
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+from aws_cdk import (
+    aws_s3 as _s3,    
+    core
+)
 
 class PracticeStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # The code that defines your stack goes 
+        
+        _s3.Bucket(
+            self,
+            "vijay782021",
+            bucket_name="vijaytestbucket782021",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+            
+        )
